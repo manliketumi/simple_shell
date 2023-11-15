1ece81563f55ec497d998262edf4dac8ebfc5df2
manliketumi <mongwegeloa@gmail.com>
2023-11-15 00:49:51 -0800

    simple shell

--- shell.h
+++ shell.h
@@ -16,19 +16,16 @@
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
+#define MAX_COMMAND_LEN 100
+#define PROMPT "$ "
+
 int _isalpha(int c);
 int _atoi(char *s);
 
 int _putchar(char c);
-int _setenv(const char *name, const char *value, int overwrite);
-
-char *_strncpy(char *dest, char *src, int n);
-char *_strchr(char *s, char c);
-char *_strncat(char *dest, char *src, int n);
-
-int _strcmp(char *s1, char *s2);
-int _strlen(char *s);
-char *_strcat(char *dest, char *src);
-char *read_line(void);
+int main();
+void print_prompt(void);
+void execute_command(const char *command);
+char *read_command(void);
 
 #endif
---
+++ simple_shell.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+/**
+ * print_prompt - Entry point
+ *
+ * Return: void
+ */
+void print_prompt(void)
+{
+	printf(PROMPT);
+}
+
+/**
+ * read_command - function reads a command from user
+ *
+ * Return: NULL on EOF
+ */
+char *read_command(void)
+{
+	static char command[MAX_COMMAND_LEN];
+
+	if (fgets(command, MAX_COMMAND_LEN, stdin) == NULL)
+	{
+		printf("\n");
+		return (NULL);
+	}
+	command[strcspn(command, "\n")] = 0; /* Removes newline */
+	return (command);
+}
+/**
+ * execute_command - function executes the given command
+ * @command: the command to be executed
+ *
+ * Return: void
+ */
+void execute_command(const char *command)
+{
+	char *argv[2];
+	char *envp[] = { NULL };
+	pid_t pid;
+
+	argv[0] = (char *)command; 
+	argv[1] = NULL;
+
+	pid = fork();
+
+	if (pid == 0)
+	{
+		if (execve(command, argv, envp) == -1)
+		{
+			perror("Error executing command");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid < 0)
+	{
+		perror("Error forking");
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
+/**
+ * main - function orchestrates these calls
+ * @argc: argument command
+ * @argv: argument value of the prompt
+ * Return: 0 on success
+ */
+int main()
+{
+	char *command;
+
+	while (1)
+	{
+		print_prompt();
+		command = read_command();
+
+		if (command == NULL)
+		break;
+
+		if (strlen(command) == 0)
+		continue;
+
+		execute_command(command);
+	}
+	return (0);
+}
